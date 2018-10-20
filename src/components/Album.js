c5c8c9e33f4652c613932a99849a16662104ea89
Diana <dspeicher27@gmail.com>
2018-10-20 15:12:24 +1000

    Checkpoint 5: Completed - Added Album Data & Linked Library Page to Each Album

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => <section className="album">Album Will Go Here</section>;
-
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+        {this.props.match.params.slug}
+        Album Will Go Here
+      </section>
+    );
+  }
+}
 export default Album;
